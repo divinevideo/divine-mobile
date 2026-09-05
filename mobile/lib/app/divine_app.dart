155397419e5b9847ffeafa4c64d6c0549b4ba411
Daniel Cadenas <dcadenas@gmail.com>
2026-09-05 00:47:20 -0300

    fix(notifications): route campaigns and preserve read rollback

--- mobile/lib/app/divine_app.dart
+++ mobile/lib/app/divine_app.dart
@@ -430,6 +430,8 @@ class _DivineAppState extends ConsumerState<DivineApp>
               eventId: tapEvent.eventId,
               notificationType: tapEvent.notificationType,
               senderPubkey: tapEvent.senderPubkey,
+              tapTargetType: tapEvent.tapTargetType,
+              tapTargetValue: tapEvent.tapTargetValue,
               container: container,
             ),
           );
