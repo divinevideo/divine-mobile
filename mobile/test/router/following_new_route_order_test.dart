3ca2af1ba07bc22fabedd9f29f1f78c7e8934abd
Daniel Cadenas <dcadenas@gmail.com>
2026-09-05 02:40:19 -0300

    test(router): pin campaign route ordering

--- mobile/test/router/following_new_route_order_test.dart
+++ mobile/test/router/following_new_route_order_test.dart
@@ -1,6 +1,8 @@
 // ABOUTME: Proves the campaign route beats /following/:pubkey in real routes.
 // ABOUTME: This guards cross-module route ordering without app initialization.
 
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:go_router/go_router.dart';
 import 'package:openvine/router/route_paths.dart';
@@ -22,6 +24,13 @@ void main() {
 
       expect(matches.isError, isFalse);
       expect(matches.fullPath, RoutePaths.followingNew);
+
+      final appRouter = File('lib/router/app_router.dart').readAsStringSync();
+      expect(
+        appRouter.indexOf('...profileRoutes()'),
+        greaterThan(appRouter.indexOf('...shellRoutes()')),
+        reason: 'The literal shell route must precede /following/:pubkey.',
+      );
     });
   });
 }
